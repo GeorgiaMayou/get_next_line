221338f5d7683408904986a04a2daf23d30c7461
Georgia MAYOU <gmayou@c3r1s5.wethinkcode.co.za>
2019-07-02 10:26:54 +0200

    Header file created for get_next_Line

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmayou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/02 10:11:55 by gmayou            #+#    #+#             */
+/*   Updated: 2019/07/02 10:12:37 by gmayou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+
+# define BUFF_SIZE 100
+
+int get_next_line(const int fd, char **line);
+
+#endif
