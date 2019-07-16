62e1defc586550ad509ad6a2e1aee16cc3b2e61e
Georgia Mayou <georgia@Georgias-MacBook-Pro.local>
2019-07-16 11:43:32 +0200

    EOF handling

--- get_next_line.c
+++ get_next_line.c
@@ -38,16 +38,21 @@ static int	ft_nlbuff(char **holder, char *buff, char **line)
 int 		get_next_line(const int fd, char **line)
 {
 	char 		buff[BUFF_SIZE + 1];
-	static char *mybuff;
+	static char *stat;
 	int			n;
 
-	if (!mybuff)
-		mybuff = ft_strnew(0);
+	if (!stat)
+		stat = ft_strnew(0);
 	if (fd < 0 || !line)
 		return (-1);
 	while ((n = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		if (ft_nlbuff(&mybuff, buff, line) == 1)
+		if (n < BUFF_SIZE)
+		{
+			*line = ft_strjoin(*line, buff);
+			return (1);
+		}
+		if (ft_nlbuff(&stat, buff, line) == 1)
 			return (1);
 	}
 	return (0);
